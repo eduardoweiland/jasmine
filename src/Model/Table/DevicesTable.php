2610e43901820fc52c3845999fb699d7a6286ce6
Eduardo Weiland <eduardo@eduardoweiland.info>
2015-10-29 00:25:22 -0200

    Método para busca de dispositivos que devem ser atualizados

--- src/Model/Table/DevicesTable.php
+++ src/Model/Table/DevicesTable.php
@@ -1,9 +1,8 @@
 <?php
+
 namespace App\Model\Table;
 
-use App\Model\Entity\Device;
 use Cake\ORM\Query;
-use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
@@ -27,7 +26,21 @@ class DevicesTable extends Table
         $this->table('devices');
         $this->displayField('id');
         $this->primaryKey('id');
+    }
 
+    /**
+     * Find a list of devices that need to be updated via SNMP.
+     *
+     * This list is based on each device update interval and last updated time.
+     *
+     * @return Query
+     */
+    public function findUpdatePending()
+    {
+        $query = $this->find();
+        $condition = $query->newExpr('DATE_ADD(last_updated, INTERVAL update_interval MINUTE) <= NOW()');
+        $query->where($condition);
+        return $query->select();
     }
 
     /**
@@ -39,24 +52,24 @@ class DevicesTable extends Table
     public function validationDefault(Validator $validator)
     {
         $validator
-            ->add('id', 'valid', ['rule' => 'numeric'])
-            ->allowEmpty('id', 'create');
+                ->add('id', 'valid', ['rule' => 'numeric'])
+                ->allowEmpty('id', 'create');
 
         $validator
-            ->add('update_interval', 'valid', ['rule' => 'numeric'])
-            ->requirePresence('update_interval', 'create')
-            ->notEmpty('update_interval');
+                ->add('update_interval', 'valid', ['rule' => 'numeric'])
+                ->requirePresence('update_interval', 'create')
+                ->notEmpty('update_interval');
 
         $validator
-            ->add('last_updated', 'valid', ['rule' => 'datetime'])
-            ->allowEmpty('last_updated');
+                ->add('last_updated', 'valid', ['rule' => 'datetime'])
+                ->allowEmpty('last_updated');
 
         $validator
-            ->requirePresence('ip_address', 'create')
-            ->notEmpty('ip_address');
+                ->requirePresence('ip_address', 'create')
+                ->notEmpty('ip_address');
 
         $validator
-            ->allowEmpty('snmp_community');
+                ->allowEmpty('snmp_community');
 
         return $validator;
     }
