ea02f74fc7dba2b84c86c7df5bfe139f6b3ada86
Eduardo Weiland <eduardo@eduardoweiland.info>
2015-11-27 20:43:18 -0200

    Redirecionar home para lista de dispositivos

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -27,39 +27,13 @@ use Cake\View\Exception\MissingTemplateException;
  */
 class PagesController extends AppController
 {
-
     /**
      * Displays a view
      *
-     * @return void|\Cake\Network\Response
-     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+     * @return \Cake\Network\Response|null
      */
     public function display()
     {
-        $path = func_get_args();
-
-        $count = count($path);
-        if (!$count) {
-            return $this->redirect('/');
-        }
-        $page = $subpage = null;
-
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
-
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $e) {
-            if (Configure::read('debug')) {
-                throw $e;
-            }
-            throw new NotFoundException();
-        }
+        return $this->redirect(['controller' => 'Devices', 'action' => 'index']);
     }
 }
